7e7cf34c3d614c6e26750e2f233cc40c692113e9
fmasouza <fabmacsouza@gmail.com>
2020-12-12 20:33:26 -0300

    Add loading on product listing

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { ProductsResponse } from 'core/types/Product';
 import { useHistory } from 'react-router-dom';
-import Card from '../Card';
-import { makePrivateRequest, makeRequest } from 'core/utils/request';
-import Pagination from 'core/components/Pagination';
 import { toast } from 'react-toastify';
+import { makePrivateRequest, makeRequest } from 'core/utils/request';
+import Card from '../Card';
+import Pagination from 'core/components/Pagination';
+import CardLoader from '../Loaders/ProductCardLoaders';
 
 const List = () => {
 
@@ -57,9 +58,11 @@ const List = () => {
                 ADICIONAR
             </button>
             <div className="admin-list-container">
-                {productsResponse?.content.map(product => (
-                    <Card product={product} key={product.id} onRemove={onRemove}/>
-                ))}
+                {isLoading ? <CardLoader /> : (
+                    productsResponse?.content.map(product => (
+                        <Card product={product} key={product.id} onRemove={onRemove}/>
+                    ))
+                )}
                 {productsResponse && (
                     <Pagination
                         totalPages={productsResponse.totalPages}
